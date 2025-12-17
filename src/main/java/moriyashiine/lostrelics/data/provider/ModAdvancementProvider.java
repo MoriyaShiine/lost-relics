282432f6a0d1148ad45ccc60c3c945669c2ab457
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-12-17 16:10:19 -0600

    add relic advancements

--- src/main/java/moriyashiine/lostrelics/data/provider/ModAdvancementProvider.java
+++ src/main/java/moriyashiine/lostrelics/data/provider/ModAdvancementProvider.java
@@ -4,11 +4,13 @@
 package moriyashiine.lostrelics.data.provider;
 
 import moriyashiine.lostrelics.common.LostRelics;
+import moriyashiine.lostrelics.common.init.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.AdvancementFrame;
+import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.advancement.criterion.TickCriterion;
 import net.minecraft.item.Items;
 import net.minecraft.predicate.entity.LocationPredicate;
@@ -28,7 +30,7 @@ public class ModAdvancementProvider extends FabricAdvancementProvider {
 
 	@Override
 	public void generateAdvancement(RegistryWrapper.WrapperLookup registryLookup, Consumer<AdvancementEntry> consumer) {
-		Advancement.Builder.create()
+		AdvancementEntry findJunglePyramid = Advancement.Builder.create()
 				.parent(Identifier.tryParse("adventure/root"))
 				.display(Items.MOSSY_COBBLESTONE,
 						Text.translatable("advancements.lostrelics.adventure.find_jungle_pyramid.title"),
@@ -40,5 +42,54 @@ public class ModAdvancementProvider extends FabricAdvancementProvider {
 						false)
 				.criterion("jungle_pyramid", TickCriterion.Conditions.createLocation(LocationPredicate.Builder.createStructure(registryLookup.getOrThrow(RegistryKeys.STRUCTURE).getOrThrow(StructureKeys.JUNGLE_PYRAMID))))
 				.build(consumer, LostRelics.id("adventure/find_jungle_pyramid").toString());
+
+		Advancement.Builder.create()
+				.parent(findJunglePyramid)
+				.display(ModItems.CURSED_AMULET,
+						Text.translatable("advancements.lostrelics.adventure.cursed_amulet.title"),
+						Text.translatable("advancements.lostrelics.adventure.cursed_amulet.description"),
+						null,
+						AdvancementFrame.TASK,
+						true,
+						true,
+						false)
+				.criterion("has_relic", InventoryChangedCriterion.Conditions.items(ModItems.CURSED_AMULET))
+				.build(consumer, LostRelics.id("adventure/cursed_amulet").toString());
+		Advancement.Builder.create()
+				.parent(findJunglePyramid)
+				.display(ModItems.SMOKING_MIRROR,
+						Text.translatable("advancements.lostrelics.adventure.smoking_mirror.title"),
+						Text.translatable("advancements.lostrelics.adventure.smoking_mirror.description"),
+						null,
+						AdvancementFrame.TASK,
+						true,
+						true,
+						false)
+				.criterion("has_relic", InventoryChangedCriterion.Conditions.items(ModItems.SMOKING_MIRROR))
+				.build(consumer, LostRelics.id("adventure/smoking_mirror").toString());
+		Advancement.Builder.create()
+				.parent(findJunglePyramid)
+				.display(ModItems.TRIPLE_TOOTHED_SNAKE,
+						Text.translatable("advancements.lostrelics.adventure.triple_toothed_snake.title"),
+						Text.translatable("advancements.lostrelics.adventure.triple_toothed_snake.description"),
+						null,
+						AdvancementFrame.TASK,
+						true,
+						true,
+						false)
+				.criterion("has_relic", InventoryChangedCriterion.Conditions.items(ModItems.TRIPLE_TOOTHED_SNAKE))
+				.build(consumer, LostRelics.id("adventure/triple_toothed_snake").toString());
+		Advancement.Builder.create()
+				.parent(findJunglePyramid)
+				.display(ModItems.TURQUOISE_EYE,
+						Text.translatable("advancements.lostrelics.adventure.turquoise_eye.title"),
+						Text.translatable("advancements.lostrelics.adventure.turquoise_eye.description"),
+						null,
+						AdvancementFrame.TASK,
+						true,
+						true,
+						false)
+				.criterion("has_relic", InventoryChangedCriterion.Conditions.items(ModItems.TURQUOISE_EYE))
+				.build(consumer, LostRelics.id("adventure/turquoise_eye").toString());
 	}
 }
