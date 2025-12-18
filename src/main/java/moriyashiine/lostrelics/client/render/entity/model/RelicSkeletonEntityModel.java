e57af0a48ffc9f077d4b00184699fdf40a4f9808
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-12-18 15:35:09 -0600

    clean up relic skeleton model

--- src/main/java/moriyashiine/lostrelics/client/render/entity/model/RelicSkeletonEntityModel.java
+++ src/main/java/moriyashiine/lostrelics/client/render/entity/model/RelicSkeletonEntityModel.java
@@ -20,18 +20,18 @@ public class RelicSkeletonEntityModel extends BipedEntityModel<RelicSkeletonEnti
 	public static TexturedModelData getTexturedModelData() {
 		ModelData data = BipedEntityModel.getModelData(Dilation.NONE, 0);
 		ModelPartData root = data.getRoot();
-		ModelPartData head = root.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(86, 0).cuboid(-3.5F, -7.5F, -4.75F, 7, 6, 7, Dilation.NONE)
-				.uv(104, 15).cuboid(-2, -1.5F, -4.75F, 4, 1, 1, new Dilation(0.01F))
-				.uv(86, 13).cuboid(-4, -6.5F, -4.8F, 8, 4, 1, new Dilation(0.1F)), ModelTransform.NONE);
-		head.addChild("lowerJaw", ModelPartBuilder.create().uv(86, 19).cuboid(-3, -1.6494F, -4.4939F, 6, 2, 6, Dilation.NONE), ModelTransform.of(0, -0.25F, 0, 0.0873F, 0, 0));
-		root.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(87, 45).cuboid(-4, 0, -2, 8, 8, 4, new Dilation(-0.01F))
-				.uv(113, 44).cuboid(-1, -1.75F, 1, 2, 12, 1, Dilation.NONE)
-				.uv(87, 58).cuboid(-4, 9, -2, 8, 3, 4, new Dilation(-0.01F)), ModelTransform.NONE);
-		root.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(84, 29).cuboid(-1, -2, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(5, 2, 0));
-		root.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(84, 29).mirrored().cuboid(-1, -2, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(-5, 2, 0));
-		root.addChild(EntityModelPartNames.LEFT_LEG, ModelPartBuilder.create().uv(96, 29).cuboid(-1, 0, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(1.9F, 12, 0));
-		root.addChild(EntityModelPartNames.RIGHT_LEG, ModelPartBuilder.create().uv(96, 29).mirrored().cuboid(-1, 0, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(-1.9F, 12, 0));
+		ModelPartData head = root.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(0, 0).cuboid(-3.5F, -7.5F, -4.35F, 7, 6, 7, Dilation.NONE)
+				.uv(20, 15).cuboid(-2, -1.5F, -4.15F, 4, 1, 1, new Dilation(0.01F))
+				.uv(0, 14).cuboid(-4, -6.5F, -4.6F, 8, 4, 1, new Dilation(0.1F)), ModelTransform.NONE);
+		head.addChild("lowerJaw", ModelPartBuilder.create().uv(4, 21).cuboid(-3, -1.5F, -3.6F, 6, 2, 4, Dilation.NONE), ModelTransform.of(0, -0.25F, 0, 0.0873F, 0, 0));
+		root.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(0, 45).cuboid(-4, 0, -2, 8, 8, 4, new Dilation(-0.01F))
+				.uv(55, 0).cuboid(-1, -1.75F, 1, 2, 12, 1, Dilation.NONE)
+				.uv(30, 0).cuboid(-4, 9, -2, 8, 3, 4, new Dilation(-0.01F)), ModelTransform.NONE);
+		root.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(0, 29).cuboid(-1, -2, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(5, 2, 0));
+		root.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(0, 29).mirrored().cuboid(-1, -2, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(-5, 2, 0));
+		root.addChild(EntityModelPartNames.LEFT_LEG, ModelPartBuilder.create().uv(12, 29).cuboid(-1, 0, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(1.9F, 12, 0));
+		root.addChild(EntityModelPartNames.RIGHT_LEG, ModelPartBuilder.create().uv(12, 29).mirrored().cuboid(-1, 0, -1, 2, 12, 2, Dilation.NONE), ModelTransform.origin(-1.9F, 12, 0));
 		head.addChild(EntityModelPartNames.HAT, ModelPartBuilder.create(), ModelTransform.NONE);
-		return TexturedModelData.of(data, 128, 128);
+		return TexturedModelData.of(data, 64, 64);
 	}
 }
