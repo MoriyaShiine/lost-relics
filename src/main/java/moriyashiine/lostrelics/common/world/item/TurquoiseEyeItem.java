e5babc7d8a7e16ff61432f4dbd64c946cce7f4bd
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-05-05 17:14:49 -0500

    use ohmega api instead of mixin

--- src/main/java/moriyashiine/lostrelics/common/world/item/TurquoiseEyeItem.java
+++ src/main/java/moriyashiine/lostrelics/common/world/item/TurquoiseEyeItem.java
@@ -6,9 +6,13 @@ package moriyashiine.lostrelics.common.world.item;
 
 import com.swacky.ohmega.api.common.item.EquipContext;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.NeutralMob;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.item.ItemStack;
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 public class TurquoiseEyeItem extends ToggleableRelicItem {
 	public TurquoiseEyeItem(Properties properties) {
@@ -18,5 +22,10 @@ public class TurquoiseEyeItem extends ToggleableRelicItem {
 	@Override
 	public void onEquip(@NonNull LivingEntity entity, @NonNull ItemStack stack, @NonNull EquipContext context) {
 		entity.removeEffect(MobEffects.INVISIBILITY);
+	}
+
+	@Override
+	public double getMobVisibilityMultiplier(@NonNull ItemStack stack, @Nullable Entity targetingEntity) {
+		return targetingEntity == null || targetingEntity instanceof Enemy || targetingEntity instanceof NeutralMob ? 2 : 1;
 	}
 }
