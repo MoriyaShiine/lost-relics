07317a74a7919692ebc536b8add76e3d4c7a843b
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-09-04 18:35:59 -0500

    fix altar item rendering being laggy

--- common/src/main/java/moriyashiine/lostrelics/client/renderer/blockentity/AltarRenderer.java
+++ common/src/main/java/moriyashiine/lostrelics/client/renderer/blockentity/AltarRenderer.java
@@ -3,6 +3,7 @@ package moriyashiine.lostrelics.client.renderer.blockentity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import it.unimi.dsi.fastutil.HashCommon;
+import moriyashiine.lostrelics.client.event.AltarClientEvent;
 import moriyashiine.lostrelics.client.renderer.blockentity.state.AltarRenderState;
 import moriyashiine.lostrelics.common.world.level.block.entity.AltarBlockEntity;
 import net.minecraft.client.renderer.SubmitNodeCollector;
@@ -12,12 +13,12 @@ import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.Nullable;
 
-public record AltarRenderer(
-		ItemModelResolver itemModelResolver) implements BlockEntityRenderer<AltarBlockEntity, AltarRenderState> {
+public record AltarRenderer(ItemModelResolver itemModelResolver) implements BlockEntityRenderer<AltarBlockEntity, AltarRenderState> {
 	public AltarRenderer(BlockEntityRendererProvider.Context context) {
 		this(context.itemModelResolver());
 	}
@@ -42,6 +43,6 @@ public record AltarRenderer(
 	public void extractRenderState(AltarBlockEntity blockEntity, AltarRenderState state, float partialTicks, Vec3 cameraPosition, ModelFeatureRenderer.@Nullable CrumblingOverlay breakProgress) {
 		BlockEntityRenderer.super.extractRenderState(blockEntity, state, partialTicks, cameraPosition, breakProgress);
 		itemModelResolver.updateForTopItem(state.stackState, blockEntity.getStack(), ItemDisplayContext.GROUND, blockEntity.getLevel(), null, HashCommon.long2int(blockEntity.getBlockPos().asLong()));
-		state.rotation = (int) blockEntity.getBlockPos().asLong() + blockEntity.getLevel().getGameTime() * 4 + partialTicks;
+		state.rotation = Mth.wrapDegrees((state.blockPos.hashCode() + AltarClientEvent.clientTime + partialTicks) * 4);
 	}
 }
